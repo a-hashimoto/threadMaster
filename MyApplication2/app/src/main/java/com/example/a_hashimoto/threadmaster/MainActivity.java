da4d96726504c32db5d5dbf9ac48fb887039956a
橋本晶 <hashimotoakira@hashimotoakira-no-MacBook-Pro.local>
2017-02-10 11:37:18 +0900

    fragmentの追加
    mainActivityにbuttonを二個設置
    
    fragmentにbuttonを一個設置

--- MyApplication2/app/src/main/java/com/example/a_hashimoto/threadmaster/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/a_hashimoto/threadmaster/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.a_hashimoto.threadmaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.thread_button)
+    View Button;
+
+    @BindView(R.id.thread_button2)
+    View Button2;
+
+    @BindView(R.id.tab_fragment)
+    View tabFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.thread_button)
+    public void setTabFragment() {
+        tabFragment.setVisibility(View.VISIBLE);
+    }
+
+    @OnClick(R.id.thread_button2)
+    public void deleteTabFragment(){
+        tabFragment.setVisibility(View.INVISIBLE);
+    }
+
 }
