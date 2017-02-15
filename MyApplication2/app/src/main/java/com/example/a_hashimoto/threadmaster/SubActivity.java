46da6e716efe352d22e43e207fbbd9314ad20eb3
a-hashimoto <a-hashimoto@unext.jp>
2017-02-15 14:49:40 +0900

    transition追加

---
+++ MyApplication2/app/src/main/java/com/example/a_hashimoto/threadmaster/SubActivity.java
@@ -0,0 +1,17 @@
+package com.example.a_hashimoto.threadmaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by a-hashimoto on 2017/02/15.
+ */
+
+public class SubActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
